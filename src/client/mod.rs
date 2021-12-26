94e198b72123860fc32c13bc55c61dc73cf06a37
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-12-26 17:43:59 +0200

    Client: improve error handling

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,35 +1,39 @@
+use std::error::Error;
+use reqwest::blocking::{Client, Response};
+
 mod response;
 use response::SearchResponse;
+
+const GOODBROTHER_USER_AGENT: &str = "GOODBROTHER_RUST";
 
 fn parse_request_url(username: &str) -> String {
     format!("https://api.github.com/search/issues?q=user:{user}+is:pr+state:open",
         user = username,)
 }
 
-fn get_client() -> reqwest::blocking::Client {
-    let client = reqwest::blocking::Client::builder()
-    .user_agent("GOODBROTHER_RUST")
-    .build()
-    .unwrap();
+fn get_client() -> Result<Client, Box<dyn Error>> {
+    let client = Client::builder()
+    .user_agent(GOODBROTHER_USER_AGENT)
+    .build()?;
 
-    return client;
+    Ok(client)
 }
 
-fn get_pull_request_body(username: String) -> String {
-    let client = get_client();
+fn get_response(request_url: String) -> Result<Response, Box<dyn Error>> {
+    let client = get_client()?;
+    let response = client.get(request_url).send()?;
+
+    Ok(response)
+}
+
+pub fn get_pull_request_response(username: String) -> Result<SearchResponse, Box<dyn Error>> {
     let request_url = parse_request_url(&username);
-
-    let body = client.get(request_url).send().unwrap().text().unwrap();
-
-    return body;
-}
-
-pub fn get_pull_request_response(username: String) -> SearchResponse {
-    let body = get_pull_request_body(username);
+    let response = get_response(request_url)?;
+    let body = response.text()?;
     
-    let pull_requests = serde_json::from_str(&body).unwrap();
+    let pull_requests = serde_json::from_str(&body)?;
 
-    pull_requests
+    Ok(pull_requests)
 }
 
 
@@ -48,12 +52,13 @@ mod tests {
 
     #[test]
     fn gets_pull_request_response() {
-        let result = get_pull_request_response("stscoundrel".to_string());
+        let result = get_pull_request_response("stscoundrel".to_string()).unwrap();
 
         println!("{}", result.total_count);
 
         assert_eq!(result.total_count, 17);
         assert_eq!(result.items.len(), 17);
         assert_eq!(result.items[0].title, "Fixture PR for integration tests");
+        assert_eq!(result.items[0].user.login, "stscoundrel");
     }
 }
