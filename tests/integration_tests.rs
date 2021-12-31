08c8ffb2ed96fea070876f8e9ed69bc2567915e9
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-12-31 15:54:18 +0200

    Integration tests: add test for grouped prs

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,22 +1,17 @@
-use goodbrother::{get_pull_requests_by_user};
-use goodbrother::{PullRequest};
+use goodbrother::{get_pull_requests_by_user, get_grouped_pull_requests_by_user};
+use goodbrother::{PullRequest, Repository};
 
 #[test]
-fn gets_pull_requess_by_user() {
+fn gets_pull_requets_by_user() {
     let username = "stscoundrel".to_string();
     let result = get_pull_requests_by_user(username).unwrap();
 
     assert!(result.len() > 0);
 
-    for repo in result.iter() {
-        println!("{}", repo.repository);
-    }
-
     // Should always contain test PR for Goodbrother.
-    let goodbrother_prs: Vec<PullRequest> = result
+    let goodbrother_prs: Vec<&PullRequest> = result
         .iter()
         .filter(|pr| pr.repository.eq("stscoundrel/goodbrother"))
-        .map(|pr| pr.clone())
         .collect();
 
     assert!(goodbrother_prs.len() > 0);
@@ -24,4 +19,25 @@ fn gets_pull_requess_by_user() {
     assert!(goodbrother_prs[0].name.eq("Fixture PR for integration tests"));
     assert!(goodbrother_prs[0].link.eq("https://github.com/stscoundrel/goodbrother/pull/18"));
     assert_eq!(goodbrother_prs[0].is_dependabot, false);
+}
+
+#[test]
+fn gets_grouped_pull_requests_by_user() {
+    let username = "stscoundrel".to_string();
+    let result = get_grouped_pull_requests_by_user(username).unwrap();
+
+    assert!(result.len() > 0);
+
+    // Should always contain prs for Goodbrother
+    let goodbrother_prs: Vec<&Repository> = result
+        .iter()
+        .filter(|repo| repo.name.eq("stscoundrel/goodbrother"))
+        .collect();
+
+    assert!(goodbrother_prs.len() > 0);
+
+    assert!(goodbrother_prs[0].name.eq("stscoundrel/goodbrother"));
+    assert!(goodbrother_prs[0].pull_requests[0].name.eq("Fixture PR for integration tests"));
+    assert!(goodbrother_prs[0].pull_requests[0].link.eq("https://github.com/stscoundrel/goodbrother/pull/18"));
+    assert_eq!(goodbrother_prs[0].pull_requests[0].is_dependabot, false);
 }
