bd8646365d8fd0ed26a6ef812b060d872a8b0c88
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-12-31 15:22:05 +0200

    Add public exports, hide internals

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,31 @@
-pub mod client;
-pub mod github;
+use std::error::Error;
+
+mod client;
+mod github;
+
+use client::{get_pull_request_response, API_URL};
+use github::{from_responses, to_repository_summary};
+pub use github::{Repository, PullRequest};
+
+/// List open PRs in Github by user
+/// 
+/// One list item per PR.
+///
+///
+pub fn get_pull_requests_by_user(username: String) -> Result<Vec<PullRequest>, Box<dyn Error>> {
+    let pull_request_response = get_pull_request_response(username, API_URL)?;
+
+    Ok(from_responses(pull_request_response.items))
+}
+
+/// List open PRs in Github by user, grouped by repository
+/// 
+/// One list item per repository that has pull requests.
+///
+///
+pub fn get_grouped_pull_requests_by_user(username: String) -> Result<Vec<Repository>, Box<dyn Error>> {
+    let pull_request_response = get_pull_request_response(username, API_URL)?;
+    let pull_requests = from_responses(pull_request_response.items);
+
+    Ok(to_repository_summary(pull_requests))
+}
