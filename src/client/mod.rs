6c93ed0aba7201897d639f5f4da1c57e0ac782e7
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-12-31 12:56:18 +0200

    GH API: use server mock for unit tests

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -6,8 +6,9 @@ use response::SearchResponse;
 
 const GOODBROTHER_USER_AGENT: &str = "GOODBROTHER_RUST";
 
-fn parse_request_url(username: &str) -> String {
-    format!("https://api.github.com/search/issues?q=user:{user}+is:pr+state:open",
+fn parse_request_url(username: &str, api_url: &str) -> String {
+    format!("{api}/search/issues?q=user:{user}+is:pr+state:open",
+        api = api_url,
         user = username,)
 }
 
@@ -26,10 +27,11 @@ fn get_response(request_url: String) -> Result<Response, Box<dyn Error>> {
     Ok(response)
 }
 
-pub fn get_pull_request_response(username: String) -> Result<SearchResponse, Box<dyn Error>> {
-    let request_url = parse_request_url(&username);
+pub fn get_pull_request_response(username: String, api_url: &str) -> Result<SearchResponse, Box<dyn Error>> {
+    let request_url = parse_request_url(&username, api_url);
     let response = get_response(request_url)?;
     let body = response.text()?;
+    println!("{}", body);
     
     let pull_requests = serde_json::from_str(&body)?;
 
@@ -40,11 +42,13 @@ pub fn get_pull_request_response(username: String) -> Result<SearchResponse, Box
 #[cfg(test)]
 mod tests {
     use super::*;
+    use httpmock::prelude::*;
+    use serde_json::json;
 
     #[test]
     fn parses_request_url() {
-        let result1 = parse_request_url("stscoundrel");
-        let result2 = parse_request_url("someoneelse");
+        let result1 = parse_request_url("stscoundrel", "https://api.github.com");
+        let result2 = parse_request_url("someoneelse", "https://api.github.com");
 
         assert_eq!(result1, "https://api.github.com/search/issues?q=user:stscoundrel+is:pr+state:open");
         assert_eq!(result2, "https://api.github.com/search/issues?q=user:someoneelse+is:pr+state:open");
@@ -52,13 +56,55 @@ mod tests {
 
     #[test]
     fn gets_pull_request_response() {
-        let result = get_pull_request_response("stscoundrel".to_string()).unwrap();
+        // Setup mock response for API to send.
+        let mock_response = json!(
+            {
+                "total_count": 2,
+                "items": [
+                    {
+                        "id": 123456,
+                        "title": "Unit Test PR",
+                        "user": {
+                            "login": "RustyMock",
+                        },
+                        "html_url": "https://github.com/stscoundrel/goodbrother-rust/pulls/666",
+                        "repository_url": "https://github.com/stscoundrel/goodbrother-rust",
+                    },
+                    {
+                        "id": 654321,
+                        "title": "Secondary Unit Test PR",
+                        "user": {
+                            "login": "RustyMock",
+                        },
+                        "html_url": "https://github.com/stscoundrel/goodbrother-rust/pulls/667",
+                        "repository_url": "https://github.com/stscoundrel/goodbrother-rust",
+                    }
+                ]
+            }
+        );
 
-        println!("{}", result.total_count);
 
-        assert_eq!(result.total_count, 17);
-        assert_eq!(result.items.len(), 17);
-        assert_eq!(result.items[0].title, "Fixture PR for integration tests");
-        assert_eq!(result.items[0].user.login, "stscoundrel");
+        let server = MockServer::start();
+        let github_mock = server.mock(|when, then| {
+            when.method(GET)
+                .path("/search/issues")
+                .query_param("q", "user:stscoundrel+is:pr+state:open");
+            then.status(200)
+                .header("content-type", "text/html")
+                .json_body(mock_response);
+        });
+
+        let result = get_pull_request_response("stscoundrel".to_string(), &server.base_url()).unwrap();
+
+        // Assert JSON reply was parsed.
+        assert_eq!(result.total_count, 2);
+        assert_eq!(result.items.len(), 2);
+        assert_eq!(result.items[0].title, "Unit Test PR");
+        assert_eq!(result.items[0].user.login, "RustyMock");
+        assert_eq!(result.items[0].html_url, "https://github.com/stscoundrel/goodbrother-rust/pulls/666");
+        assert_eq!(result.items[0].repository_url, "https://github.com/stscoundrel/goodbrother-rust");
+
+        // Assert it came from the mock server.
+        github_mock.assert()
     }
 }
