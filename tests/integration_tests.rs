95124fd292e67bb260db4216b6c297039e6e9758
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-01-06 10:44:24 +0200

    Integration test: expect fixture PR to always be the last
    Last = oldest.

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -16,9 +16,9 @@ fn gets_pull_requets_by_user() {
 
     assert!(goodbrother_prs.len() > 0);
 
-    assert!(goodbrother_prs[0].name.eq("Fixture PR for integration tests"));
-    assert!(goodbrother_prs[0].link.eq("https://github.com/stscoundrel/goodbrother-rust/pull/16"));
-    assert_eq!(goodbrother_prs[0].is_dependabot, false);
+    assert!(goodbrother_prs.last().unwrap().name.eq("Fixture PR for integration tests"));
+    assert!(goodbrother_prs.last().unwrap().link.eq("https://github.com/stscoundrel/goodbrother-rust/pull/16"));
+    assert_eq!(goodbrother_prs.last().unwrap().is_dependabot, false);
 }
 
 #[test]
@@ -37,7 +37,7 @@ fn gets_grouped_pull_requests_by_user() {
     assert!(goodbrother_prs.len() > 0);
 
     assert!(goodbrother_prs[0].name.eq("stscoundrel/goodbrother-rust"));
-    assert!(goodbrother_prs[0].pull_requests[0].name.eq("Fixture PR for integration tests"));
-    assert!(goodbrother_prs[0].pull_requests[0].link.eq("https://github.com/stscoundrel/goodbrother-rust/pull/16"));
-    assert_eq!(goodbrother_prs[0].pull_requests[0].is_dependabot, false);
+    assert!(goodbrother_prs[0].pull_requests.last().unwrap().name.eq("Fixture PR for integration tests"));
+    assert!(goodbrother_prs[0].pull_requests.last().unwrap().link.eq("https://github.com/stscoundrel/goodbrother-rust/pull/16"));
+    assert_eq!(goodbrother_prs[0].pull_requests.last().unwrap().is_dependabot, false);
 }
