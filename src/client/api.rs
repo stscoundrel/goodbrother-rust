8987a4d3f441c65b9f22b875366006e75982938e
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-11-05 17:08:31 +0200

    GH client: ignore archived repos
    Closes #25

--- src/client/api.rs
+++ src/client/api.rs
@@ -4,7 +4,7 @@ use reqwest::blocking::{Client, Response};
 const GOODBROTHER_USER_AGENT: &str = "GOODBROTHER_RUST";
 
 pub fn parse_request_url(username: &str, api_url: &str, page: i8) -> String {
-    format!("{api}/search/issues?q=user:{user}+is:pr+state:open&per_page=100&page={page}",
+    format!("{api}/search/issues?q=user:{user}+is:pr+state:open+archived:false&per_page=100&page={page}",
         api = api_url,
         user = username,
         page = page)
@@ -35,7 +35,7 @@ mod tests {
         let result1 = parse_request_url("stscoundrel", "https://api.github.com", 1);
         let result2 = parse_request_url("someoneelse", "https://api.github.com", 2);
 
-        assert_eq!(result1, "https://api.github.com/search/issues?q=user:stscoundrel+is:pr+state:open&per_page=100&page=1");
-        assert_eq!(result2, "https://api.github.com/search/issues?q=user:someoneelse+is:pr+state:open&per_page=100&page=2");
+        assert_eq!(result1, "https://api.github.com/search/issues?q=user:stscoundrel+is:pr+state:open+archived:false&per_page=100&page=1");
+        assert_eq!(result2, "https://api.github.com/search/issues?q=user:someoneelse+is:pr+state:open+archived:false&per_page=100&page=2");
     }
 }
